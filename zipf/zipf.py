3b13d779a1e24eb947126479834855f7ce97c735
Luca Cappelletti <cappelletti.luca94@gmail.com>
2018-04-17 11:15:02 +0200

    Implemented JSD divergence

--- zipf/zipf.py
+++ zipf/zipf.py
@@ -1,12 +1,12 @@
 from .factories.from_dir.from_dir import from_dir
 from multiprocessing import Process, cpu_count
 from collections import OrderedDict
-import math
+import math, numbers
 import matplotlib.pyplot as plt
 import json
 
 class zipf:
-    def __init__(self, data):
+    def __init__(self, data={}):
         self._data = OrderedDict(data)
 
     def from_dir(path, file_interface=None, word_filter=None, output_file=None, use_cli=False):
@@ -44,18 +44,45 @@ class zipf:
     def __setitem__(self, key, value):
         self._data[key] = value
 
+    def __mul__(self, value):
+        if isinstance(value, numbers.Number):
+            tmp_zipf = zipf()
+            tmp_zipf.update((x, y*value) for x, y in self.items())
+            return tmp_zipf
+        else:
+            raise ValueError("Moltiplication is allowed only with int and floats.")
+
+    __rmul__ = __mul__
+
+    def __add__(self, other):
+        return zipf({ k: self.get(k) + other.get(k) for k in set(self) | set(other) })
+
     def KL(self, other):
+        """Kullback–Leibler divergence defined for subset"""
+        """https://en.wikipedia.org/wiki/Kullback%E2%80%93Leibler_divergence"""
         total = 0
         for key in set(self.keys()) & set(other.keys()):
             v = self[key]
             total += v*math.log(v/other[key])
         return total
 
+    def JSD(self, other):
+        """Jensen–Shannon divergence"""
+        """https://en.wikipedia.org/wiki/Jensen%E2%80%93Shannon_divergence"""
+        M = 0.5*(self + other)
+        return 0.5*(self.KL(M) + other.KL(M))
+
+    def get(self, key, default=0):
+        return self._data.get(key, default)
+
     def items(self):
         return self._data.items()
 
     def keys(self):
         return self._data.keys()
+
+    def update(self, value):
+        return self._data.update(value)
 
     def plot(self):
         y = [t[1] for t in self.items()]
