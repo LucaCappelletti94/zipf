08fdebb370c81391e2b5df16949b843bf3342e24
Luca Cappelletti <cappelletti.luca94@gmail.com>
2018-04-19 12:21:43 +0200

    Started to create batch operations

---
+++ zipf/batchs/jsd.py
@@ -0,0 +1,12 @@
+from multiprocessing import
+
+class jsd:
+    def __init__(self, path, zipfs, output, use_cli = False):
+        self._path = path
+        self._zipfs = zipfs
+        self._output = output
+        self._use_cli = use_cli
+
+    def job(self):
+
+    def run(self):
