d2fc45f215bb74fd501fe59c8bf35be8b593048b
Luca Cappelletti <cappelletti.luca94@gmail.com>
2018-04-22 11:41:49 +0200

    Added zipf_from_url factory

--- zipf/__init__.py
+++ zipf/__init__.py
@@ -1,2 +1,2 @@
 from .zipf import zipf
-from .factories import zipf_from_dir, zipf_from_file, zipf_from_text
+from .factories import zipf_from_list, zipf_from_dir, zipf_from_file, zipf_from_text, zipf_from_url
