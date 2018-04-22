e18e71e8a6a6c6144a4f14233eb1151d920e9525
Luca Cappelletti <cappelletti.luca94@gmail.com>
2018-04-22 22:22:31 +0200

    Updated version number

--- zipf/setup.py
+++ zipf/setup.py
@@ -15,7 +15,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.0.0',
+    version='1.0.1',
 
     description='A package to create and work with zipf distributions',
     long_description=long_description,
