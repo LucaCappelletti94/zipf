5ecebdc847321545b55216a0120da0d6c06929f3
Luca Cappelletti <cappelletti.luca94@gmail.com>
2018-05-03 15:18:06 +0200

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -4,13 +4,27 @@ from codecs import open
 from os import path
 import sys
 import os
-from .zipf.__version__ import __version__
+import re
 
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the relevant file
 with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
+
+def read(*parts):
+    with open(os.path.join(here, *parts), 'r') as fp:
+        return fp.read()
+
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
+__version__ = find_version("zipf", "__version__.py")
 
 setup(
     name='zipf',
