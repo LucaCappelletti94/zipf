e0e21534399ba3d4e58fb7c6c789adffe541983b
Luca Cappelletti <cappelletti.luca94@gmail.com>
2018-05-03 23:18:57 +0200

    Fixed 3 tests

--- tests/zipf/basics/div_test.py
+++ tests/zipf/basics/div_test.py
@@ -1,6 +1,10 @@
 from zipf import zipf
 
 def test_answer():
-    z1 = zipf({"a":0.5, "b":0.5})
+    z1 = zipf({"a":0.5, "b":0.25})
     z2 = zipf({"a":1, "b":1})
-    assert (z1/0.5, z1/z2) == (z2, z1)
+    assert (
+        z1/0.5, (z1/z2).sort()
+    ) == (
+        zipf({"a":1, "b":0.5}), z1
+    )
--- tests/zipf/basics/sum_test.py
+++ tests/zipf/basics/sum_test.py
@@ -2,6 +2,6 @@ from zipf import zipf
 
 def test_answer():
     z1 = zipf({"a":1})
-    z2 = zipf({"b":1})
-    z3 = zipf({"a":1, "b":1})
+    z2 = zipf({"b":0.5})
+    z3 = zipf({"a":1, "b":0.5})
     assert (z1+z2).sort() == z3
