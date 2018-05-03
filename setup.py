13a0e9d0709995ca2d4fbdf79c36fc9c06593b1f
Luca Cappelletti <cappelletti.luca94@gmail.com>
2018-05-03 15:06:35 +0200

    Updated setup requirements

--- setup.py
+++ setup.py
@@ -62,7 +62,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['requests']
+    install_requires=['requests', 'matplotlib']
 )
 
 def status(s):
