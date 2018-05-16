727b84fa57adbdec9c76385c92c1a1920d12836a
Luca Cappelletti <cappelletti.luca94@gmail.com>
2018-05-16 15:33:12 +0200

    Now testing with cli

--- tests/factories/test_dir_factory.py
+++ tests/factories/test_dir_factory.py
@@ -6,6 +6,12 @@ def prepare(options):
     factory.set_interface(lambda f: f.read())
     return factory
 
+def prepare_with_cli(options):
+    factory = ZipfFromDir(options, use_cli=True)
+    factory.set_interface(lambda f: f.read())
+    return factory
+
 def test_dir_factory():
     errors = factory_fails(ZipfFromDir, "dir", prepare)
+    errors += factory_fails(ZipfFromDir, "dir", prepare_with_cli)
     assert not errors, "errors occured:\n{}".format("\n".join(errors))
