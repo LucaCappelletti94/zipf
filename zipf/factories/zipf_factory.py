1df0ec510befb7a7bc3eda6b41172165b9f4da54
Luca Cappelletti <cappelletti.luca94@gmail.com>
2018-05-22 12:26:14 +0200

    Added docstrings

--- zipf/factories/zipf_factory.py
+++ zipf/factories/zipf_factory.py
@@ -1,10 +1,13 @@
+"""ZipfFactory creates Zipf objects."""
+import json
 import os
 from collections import defaultdict
+
 from ..zipf import Zipf
-import json
 
 
 class ZipfFactory():
+    """ZipfFactory creates Zipf objects."""
 
     _default_opts = {
         "remove_stop_words": False,
@@ -16,12 +19,13 @@ class ZipfFactory():
     }
 
     def __init__(self, options=None):
+        """Initialize a ZipfFactory with given options."""
         self._opts = {}
         self._opts.update(self._default_opts)
         if options is not None:
             self._opts.update(options)
 
-        self.validate_opts()
+        self._validate_opts()
         self._product = None
 
         if self._opts["remove_stop_words"]:
@@ -35,13 +39,13 @@ class ZipfFactory():
             self._chain = lambda elements: elements
 
     def __str__(self):
-        """Prints a json dictionary representing the Zipf"""
+        """Print a json dictionary representing the factory."""
         return json.dumps(self._opts, indent=2)
 
-    __repr__ = __str__
+    _repr_html_ = __repr__ = __str__
 
-    def validate_opts(self):
-        # Validating options types
+    def _validate_opts(self):
+        """Validate user options."""
         for opt in self._default_opts:
             if not isinstance(self._default_opts[opt], type(self._opts[opt])):
                 raise ValueError("The given option %s should have type %s." % (
@@ -54,18 +58,21 @@ class ZipfFactory():
             raise ValueError("min %s must be <= max %s" % (_min, _max))
 
     def set_product(self, product):
+        """Set the final product object."""
         self._product = product
         self._product_get = product.__getitem__
         self._product_set = product.__setitem__
 
     def get_product(self):
+        """Return the elaborated product."""
         return self._product
 
     def set_word_filter(self, _filter):
-        """Sets the function that filters words"""
+        """Set the function that filters words."""
         self._word_filter = lambda w: _filter(w) and self._stop_word_filter(w)
 
     def _load_stop_words(self):
+        """Load from path the stopwords."""
         path = os.path.join(os.path.dirname(__file__), 'stop_words.json')
         with open(path, "r") as f:
             self._stop_words = {}
@@ -73,13 +80,16 @@ class ZipfFactory():
                 self._stop_words[w] = None
 
     def _stop_word_filter(self, element):
+        """Check if word is not a stopword."""
         return element not in self._stop_words
 
-    def _filter(self, elements):
+    def _filter(self, words):
+        """Return generator of filtered words."""
         w = self._word_filter
-        return (el for el in elements if w(el))
+        return (word for word in words if w(word))
 
     def _clean(self, generator):
+        """Return a generator without low frequency words."""
         frequency = defaultdict(int)
         _min = self._opts["minimum_count"]
         for el in generator:
@@ -87,17 +97,19 @@ class ZipfFactory():
         return ((k, v) for k, v in frequency.items() if v > _min)
 
     def _chain(self, elements):
+        """Return a generator with chained words."""
         join = self._opts["chaining_character"].join
         _min = self._opts["chain_min_len"]
-        _max = self._opts["chain_max_len"]+1
+        _max = self._opts["chain_max_len"] + 1
         elements = list(elements)
         n = len(elements)
-        n1 = n+1
+        n1 = n + 1
         for i in range(n):
-            for j in range(i+_min,  min(i+_max, n1)):
+            for j in range(i + _min,  min(i + _max, n1)):
                 yield join(elements[i:j])
 
     def run(self, elements):
+        """Return a zipf created from the given elements."""
         zipf = Zipf()
         zset = zipf.__setitem__
         n = 0
@@ -110,8 +122,8 @@ class ZipfFactory():
             product_get = self._product_get
             product_set = self._product_set
             for k, v in zipf.items():
-                product_set(k, product_get(k)+v/n)
-        z = zipf/n
+                product_set(k, product_get(k) + v / n)
+        z = zipf / n
         if self._opts["sort"]:
             return z.sort()
         return z
